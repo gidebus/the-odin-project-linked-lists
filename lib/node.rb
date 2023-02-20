99232ee8710c85ab5d1924ffff3527eea9676a6f
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-02-20 18:56:00 -0500

    ADD: class Node and Tests

--- lib/node.rb
+++ lib/node.rb
@@ -2,10 +2,14 @@
 
 class Node
 
-  def value
-  end
+  attr_accessor :value, :next_node
 
-  def next_node
+  def initialize(
+    value = nil,
+    next_node = nil
+  )
+    @value = value
+    @next_node = next_node
   end
 
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -3,12 +3,18 @@
 require_relative '../lib/node'
 
 describe Node do
-  let(:node) { Node.new }
+  let(:second_node) { Node.new('second node') }
+  let(:node) { Node.new('first node', second_node) }
 
   describe '#value' do
+    it 'returns a value' do
+      expect(node.value).to eq('first node')
+    end
   end
 
   describe '#next_node' do
+    it 'returns another node' do
+      expect(node.next_node).to eq(second_node)
+    end
   end
-  
 end
